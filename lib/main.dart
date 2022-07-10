c2edba89adc077998213ccca77d313a38c2b0cc9
Gabriel Magalhaes <macedommag@gmail.com>
2022-07-10 19:55:56 -0300

    Widgets Tree

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,15 @@ main() => runApp(new PerguntaApp());
 class PerguntaApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Olá mundo"),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas") ,
+          ),
+          body: Text("Olá Mundo!!!"),
+      ),
+    );
+
   }
 
 }
