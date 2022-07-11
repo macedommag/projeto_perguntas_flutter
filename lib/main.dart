bcbe7896c165ea48e04f7cd641190cb029384652
Gabriel Magalhaes <macedommag@gmail.com>
2022-07-10 21:08:18 -0300

    app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 main() => runApp(new PerguntaApp());
@@ -5,12 +7,33 @@ main() => runApp(new PerguntaApp());
 class PerguntaApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    final List perguntas =[
+      'Qual a sua cor favorita?',
+      'Qual seu animal favorito'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Perguntas") ,
           ),
-          body: Text("Olá Mundo!!!"),
+          body: Column(
+            children: <Widget>[
+              Text(perguntas[0]),
+              RaisedButton(
+                child: Text('Resposta 1'),
+                onPressed: null,
+              ),
+              // ignore: prefer_const_constructors
+              RaisedButton(
+                child: Text('Resposta 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Resposta 3'),
+                onPressed: null,
+
+              ),
+            ],)
       ),
     );
 
